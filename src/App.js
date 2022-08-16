efced261d423971684f88f6863baae9a0149dc0b
muhammad-jiku <muhammadjiku364@gmail.com>
2022-08-16 13:59:14 +0600

    state management issues showing with props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
+import Stats from './components/Stats';
+
+const initialState = [
+  {
+    id: 1,
+    count: 0,
+  },
+  {
+    id: 2,
+    count: 0,
+  },
+];
 
 function App() {
+  const [state, setState] = useState(initialState);
+
+  const increment = (id) => {
+    const updatedIncrementCounter = state.map((c) => {
+      if (id === c.id) {
+        return {
+          ...c,
+          count: c.count + 1,
+        };
+      }
+      return { ...c };
+    });
+    setState(updatedIncrementCounter);
+  };
+
+  const decrement = (id) => {
+    const updatedDecrementCounter = state.map((c) => {
+      if (id === c.id) {
+        return {
+          ...c,
+          count: c.count - 1,
+        };
+      }
+      return { ...c };
+    });
+    setState(updatedDecrementCounter);
+  };
+  const totalCount = () => {
+    return state.reduce((total, counter) => total + counter?.count, 0);
+  };
   return (
     <div className="App">
-      <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+      <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
         {/* <!-- header --> */}
-        <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+        <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
           Simple Counter Application
         </h1>
 
         {/* <!-- counters --> */}
-        <div class="max-w-md mx-auto mt-10 space-y-5">
-          <Counter />
+        <div className="max-w-md mx-auto mt-10 space-y-5">
+          {state.map((count) => (
+            <>
+              <Counter
+                key={count?.id}
+                id={count?.id}
+                value={count?.count}
+                increment={increment}
+                decrement={decrement}
+              />
+            </>
+          ))}
+
+          <Stats count={totalCount()} />
         </div>
       </div>
     </div>
---
+++ src/components/Count.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Count = ({ value }) => {
+  return <div className="text-2xl font-semibold">{value}</div>;
+};
+
+export default Count;
